0fea8fc394ede1f07863d036797da2f61a7a3eab
useryize <80008100@sf.com>
2025-11-10 15:51:50 +0800

    菜单布局

--- src/layout/LayoutMenu/index.vue
+++ src/layout/LayoutMenu/index.vue
@@ -1,8 +1,29 @@
 <template>
-  <el-scrollbar>
-    <el-menu>
-    </el-menu>
-  </el-scrollbar>
+  <el-radio-group v-model="isCollapse" style="margin-bottom: 20px">
+    <el-radio-button :value="false">expand</el-radio-button>
+    <el-radio-button :value="true">collapse</el-radio-button>
+  </el-radio-group>
+  <el-menu default-active="1" class="el-menu-vertical-demo" :collapse="isCollapse">
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>Navigator Two</template>
+    </el-menu-item>
+  </el-menu>
 </template>
-<script setup lang="ts">
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+import {
+  Menu as IconMenu,
+} from '@element-plus/icons-vue'
+
+const isCollapse = ref(true)
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
 </script>
+
+<style></style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,17 @@
 <template>
-  <el-container class="h-[100vh] bg-[#f6f8fc]">
-    <el-aside class="h-full !w-[200px]">
+  <section class="flex h-[100vh] bg-[#f6f8fc]">
+    <div class="h-full w-fit">
       <layout-menu></layout-menu>
-    </el-aside>
-    <el-container>
-      <el-header class="!h-[90px]">header</el-header>
-      <el-main>contert</el-main>
-    </el-container>
-  </el-container>
-
+    </div>
+    <div class="h-full flex-1">
+      <LayoutHeader />
+      <LayoutMain />
+    </div>
+  </section>
 </template>
 <script setup lang="ts">
 
+import LayoutMain from './LayoutMain/index.vue'
 import LayoutMenu from './LayoutMenu/index.vue'
+import LayoutHeader from './LayoutHeader/index.vue'
 </script>
